fe1e9ff9d57d53ee589980e1467d373239cd1276
Азамат Батыркаев <folkman59@yandex.ru>
2022-06-29 03:06:24 +0500

    add db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const next = require('next');
+const { Pool, Client } = require('pg')
 
 const dev = process.env.NODE_ENV != 'production';
 const nextApp = next({ dev });
@@ -15,6 +16,19 @@ io.on('connect', socket =>{
         message: "you connected"
     })
 })
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'zernjchat',
+    password: 'postgrespw',
+    port: 49153,
+})
+
+ pool.query('SELECT * FROM public.rooms', (err, res) => {
+   console.log(err, res)
+   pool.end()
+ })
 
 nextApp.prepare().then(()=>{
     app.get('*', (req, res) => {
