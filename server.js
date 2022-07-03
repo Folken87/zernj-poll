e7121a78cc628312352cee0523ae0eac8a411f90
Азамат Батыркаев <folkman59@yandex.ru>
2022-07-03 06:00:38 +0500

    port fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const dev = process.env.NODE_ENV != 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-let port = 3000;
+let port = 80;
 
 io.on('connect', socket => {
     console.log("User connected!");
